89f19805e8872aeb75d81c5a3a24401a0aba3c16
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-07-22 11:04:02 +0200

    🆕 Synced components from Blaze Studio for cddd

---
+++ client/src/component/intake-process-verification-approval.js
@@ -0,0 +1,8 @@
+
+  import React from 'react';
+
+  const intake-process-verification-approval = () => {
+    return <div>intake-process-verification-approval view placeholder</div>;
+  };
+
+  export default intake-process-verification-approval;
