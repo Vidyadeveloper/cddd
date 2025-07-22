e8c314a659cb34c69adcff06bd11ec8c6894d1e8
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-07-22 11:07:13 +0200

    🆕 Synced components from Blaze Studio for cddd

---
+++ client/src/component/intake-process-review-serach.js
@@ -0,0 +1,8 @@
+
+  import React from 'react';
+
+  const intake-process-review-serach = () => {
+    return <div>intake-process-review-serach view placeholder</div>;
+  };
+
+  export default intake-process-review-serach;
