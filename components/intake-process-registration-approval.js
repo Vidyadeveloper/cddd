74b9b1ae29233e1a14c8231e185b9e0bd8d8d71c
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-07-22 10:49:19 +0200

    🆕 Synced components from Blaze Studio for cddd

---
+++ components/intake-process-registration-approval.js
@@ -0,0 +1,8 @@
+
+  import React from 'react';
+
+  const intake-process-registration-approval = () => {
+    return <div>intake-process-registration-approval view placeholder</div>;
+  };
+
+  export default intake-process-registration-approval;
---
+++ components/intake-process-registration-credit-history.js
@@ -0,0 +1,8 @@
+
+  import React from 'react';
+
+  const intake-process-registration-credit-history = () => {
+    return <div>intake-process-registration-credit-history view placeholder</div>;
+  };
+
+  export default intake-process-registration-credit-history;
