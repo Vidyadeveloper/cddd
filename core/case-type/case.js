ab6ff235af92df9345ec511e669c5c6c7f69a758
Vidyadeveloper <Vidyadeveloper@blaze-sync.local>
2025-07-25 14:13:10 +0200

    🔄 Synced components & case types [2025-07-25T12:13:09.474Z]

---
+++ core/case-type/case.js
@@ -0,0 +1,22 @@
+// Case Type: case
+module.exports = {
+  "caseType": {
+    "id": "case",
+    "label": "case"
+  },
+  "stages": [
+    {
+      "id": "stage1",
+      "label": "Stage 1",
+      "steps": [
+        {
+          "id": "step1",
+          "label": "Step 1",
+          "status": "pending",
+          "view": "summary",
+          "type": "manual"
+        }
+      ]
+    }
+  ]
+};
